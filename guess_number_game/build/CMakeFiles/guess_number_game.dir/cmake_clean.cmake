d7967b7e7d4f0faaa5f81c553413594f59787d9f
wangshenglongcn <wangshenglongcn@outlook.com>
2025-06-26 16:58:50 +0800

    基于猜数字版本4，增加cmake编译

---
+++ guess_number_game/build/CMakeFiles/guess_number_game.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/guess_number_game.dir/main.cpp.obj"
+  "CMakeFiles/guess_number_game.dir/main.cpp.obj.d"
+  "guess_number_game.exe"
+  "guess_number_game.exe.manifest"
+  "guess_number_game.pdb"
+  "libguess_number_game.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/guess_number_game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ guess_number_game/build/CMakeFiles/guess_number_game.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for guess_number_game.
+# This may be replaced when dependencies are built.
