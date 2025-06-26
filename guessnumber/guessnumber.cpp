6f32c6bbb719a61528b4ad49383ccc32f9461fcc
wangshenglongcn <wangshenglongcn@outlook.com>
2025-06-26 12:25:36 +0800

    实现猜数字版本2，增加统计猜的次数、次数过多推出以及重复游玩功能

--- guessnumber/guessnumber.cpp
+++ guessnumber/guessnumber.cpp
@@ -3,32 +3,52 @@
 
 using namespace std;
 
-int main() {
-    random_device rd;
-    mt19937 generator(rd());
+void playGame(mt19937& generator) {
     uniform_int_distribution<int> distribution(1, 100);
     int target = distribution(generator);
 
     cout << "I'm thinking of a number between 1 and 100.\n";
     cout << "Can you guess it?\n";
-    int num;
+
+    int num, attempts = 0;
     while (true) {
         cout << "Enter your guess: ";
         if (!(cin >> num)) {
             cout << "Please enter a valid number.\n";
-            cin.clear();  // 清除cin的错误状态
-            cin.ignore(1000, '\n');  // 清除输入的字符，目前只限制了1000字符
+            cin.clear(); // 清除cin的错误状态
+            cin.ignore(1000, '\n'); // 丢弃错误输入
             continue;
         }
+
+        attempts++;
         if (num == target) {
-            cout << num << " is correct.\n";
+            cout << num << " is correct. You guessed it in " << attempts << " tries.\n";
             break;
         } else if (num > target) {
-            cout << num << " is bigger.\n";
+            cout << num << " is too big.\n";
         } else {
-            cout << num << " is smaller.\n";
+            cout << num << " is too small.\n";
+        }
+
+        if (attempts == 10) {
+            cout << "You failed to guess the number. It was " << target << ".\n";
+            break;
         }
     }
+}
 
+int main() {
+    random_device rd;
+    mt19937 generator(rd());
+
+    char again;
+    do {
+        playGame(generator);
+        cout << "Do you want to play again? (y/n): ";
+        cin >> again;
+        cin.ignore(1000, '\n'); // 丢掉多余字符
+    } while (again == 'y' || again == 'Y');
+
+    cout << "Thanks for playing!\n";
     return 0;
 }
