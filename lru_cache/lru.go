a47b8a6e2fe3a3d6dd34d0acab54b71c72342d5c
wangshenglong <wangshenglongcn@outlook.com>
2025-06-30 11:31:57 +0800

    解决删除节点时未删除cache哈希表中key的错误，加入部分注释，修改部分命名

--- lru_cache/lru.go
+++ lru_cache/lru.go
@@ -6,14 +6,14 @@ import (
 
 // 定义双链表
 type Node struct {
-	key, val   int
-	prev, next *Node
+	key, val   int   // 存储节点对应的key和value
+	prev, next *Node // 指向前一个节点以及后一个节点的指针
 }
 
-var dummy = &Node{key: 0, val: 0}
-var cache = map[int]*Node{}
-var capacity int
-var size = 0
+var dummy = &Node{key: 0, val: 0} // dummy 是一个哨兵节点
+var cache = map[int]*Node{}       // cache 是哈希表，存储key-*Node键值对，以便能正常添加删除
+var capacity int                  // capacity 缓存最大容量
+var size = 0                      // size 记录当前节点数量
 
 func add(node *Node) {
 	// 指向右侧的指针修改
@@ -28,18 +28,21 @@ func add(node *Node) {
 }
 
 func remove(node *Node) {
+	// 改变指针指向，go是垃圾回收语言，不需要手动删除
 	node.next.prev = node.prev
 	node.prev.next = node.next
 
 	size--
 }
 
-func initial() {
+func initList() {
+	// 初始化哨兵节点指针
 	dummy.next = dummy
 	dummy.prev = dummy
 }
 
 func printNode() {
+	// 从头到尾打印当前链表内节点
 	fmt.Print("操作完成，以下为当前数据：\n")
 	cur := dummy.next
 	for i := 0; i < size; i++ {
@@ -50,20 +53,20 @@ func printNode() {
 }
 
 func main() {
-	initial()
+	initList()
 
 	fmt.Print("请输入LRU缓存的容量：")
 	fmt.Scan(&capacity)
 	for {
 		fmt.Print("请输入你想进行的操作（add/delete/quit）：")
-		var p1 string
-		fmt.Scan(&p1)
+		var op string
+		fmt.Scan(&op)
 
-		if p1 == "quit" {
+		if op == "quit" {
 			break
 		}
 
-		switch p1 {
+		switch op {
 		case "add":
 			{
 				fmt.Print("请输入一对键值对：")
@@ -80,8 +83,9 @@ func main() {
 					add(node)
 				}
 				if size > capacity {
-					remove(dummy.prev)
-					delete(cache, key)
+					last := dummy.prev
+					remove(last)
+					delete(cache, last.key)
 				}
 			}
 		case "delete":
@@ -92,6 +96,7 @@ func main() {
 				node := cache[key]
 				if node != nil {
 					remove(node)
+					delete(cache, key)
 				} else {
 					fmt.Printf("%d 不存在\n", key)
 				}
