e9822266f839f735d17586776dcaf7caa557bb08
wangshenglong <wangshenglongcn@outlook.com>
2025-06-30 11:18:40 +0800

    初步完成GO语言模拟LRU缓存

---
+++ lru_cache/lru.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"fmt"
+)
+
+// 定义双链表
+type Node struct {
+	key, val   int
+	prev, next *Node
+}
+
+var dummy = &Node{key: 0, val: 0}
+var cache = map[int]*Node{}
+var capacity int
+var size = 0
+
+func add(node *Node) {
+	// 指向右侧的指针修改
+	node.next = dummy.next
+	dummy.next = node
+
+	// 指向左侧的指针修改
+	node.prev = dummy
+	node.next.prev = node
+
+	size++
+}
+
+func remove(node *Node) {
+	node.next.prev = node.prev
+	node.prev.next = node.next
+
+	size--
+}
+
+func initial() {
+	dummy.next = dummy
+	dummy.prev = dummy
+}
+
+func printNode() {
+	fmt.Print("操作完成，以下为当前数据：\n")
+	cur := dummy.next
+	for i := 0; i < size; i++ {
+		fmt.Printf("%d %d\n", cur.key, cur.val)
+		cur = cur.next
+	}
+	fmt.Print("=======================\n")
+}
+
+func main() {
+	initial()
+
+	fmt.Print("请输入LRU缓存的容量：")
+	fmt.Scan(&capacity)
+	for {
+		fmt.Print("请输入你想进行的操作（add/delete/quit）：")
+		var p1 string
+		fmt.Scan(&p1)
+
+		if p1 == "quit" {
+			break
+		}
+
+		switch p1 {
+		case "add":
+			{
+				fmt.Print("请输入一对键值对：")
+				var key, value int
+				fmt.Scan(&key, &value)
+				node := cache[key]
+				if node == nil {
+					node = &Node{key: key, val: value}
+					add(node)
+					cache[key] = node
+				} else {
+					node.val = value
+					remove(node)
+					add(node)
+				}
+				if size > capacity {
+					remove(dummy.prev)
+					delete(cache, key)
+				}
+			}
+		case "delete":
+			{
+				fmt.Print("请输入要删除的key：")
+				var key int
+				fmt.Scan(&key)
+				node := cache[key]
+				if node != nil {
+					remove(node)
+				} else {
+					fmt.Printf("%d 不存在\n", key)
+				}
+			}
+		}
+		printNode()
+	}
+}
